506fcd238e163e09b386eaa605e12f88be20401a
Patrick <pb760@uowmail.edu.au>
2023-03-03 13:43:46 +1100

    html, css and javascript for 3droom updated

---
+++ 3droom/3droom.js
@@ -0,0 +1,5 @@
+// select button and add event listener
+document.addEventListener("click", () => {
+  // add a class which toggles the div containing table
+  document.querySelector(".table").classList.toggle("change");
+});
