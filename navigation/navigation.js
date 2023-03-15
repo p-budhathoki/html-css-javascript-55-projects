ebee60db97bb9fb0cc895b0b941fffffc48150e7
Patrick <pb760@uowmail.edu.au>
2023-03-16 10:54:29 +1100

    html, css and javascript for navigation updated

---
+++ navigation/navigation.js
@@ -0,0 +1,6 @@
+// select dots-btn and add event listener
+document.querySelector(".dots-btn").addEventListener("click", () => {
+  // add .change class to toggle the button
+  document.querySelector(".container").classList.toggle("change");
+});
+
